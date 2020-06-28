8f71074b7b934ee0217de80e5a8599d6f8b1a254
LaukeyMwaura <laukeymwaura@gmail.com>
2020-06-28 15:34:50 +0300

    Interactiveness added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,61 @@
+$("#hover1").click(function() {
+    $("#show1").show();
+    $("#hover1").hide();
+});
+
+$("#show1").click(function() {
+    $("#show1").hide();
+    $("#hover1").show();
+});
+$("#hover2").click(function() {
+    $("#show2").show();
+    $("#hover2").hide();
+});
+$("#show2").click(function() {
+    $("#show2").hide();
+    $("#hover2").show();
+});
+$("#hover3").click(function() {
+    $("#show3").show();
+    $("#hover3").hide();
+});
+$("#show3").click(function() {
+    $("#show3").hide();
+    $("#hover3").show();
+});
+$("#hover4").click(function() {
+    $("#show4").show();
+    $("#hover4").hide();
+});
+$("#show4").click(function() {
+    $("#show4").hide();
+    $("#hover4").show();
+});
+$("#hover5").click(function() {
+    $("#show5").show();
+    $("#hover5").hide();
+});
+$("#show5").click(function() {
+    $("#show5").hide();
+    $("#hover5").show();
+});
+$("#hover6").click(function() {
+    $("#show6").show();
+    $("#hover6").hide();
+});
+$("#show6").click(function() {
+    $("#show6").hide();
+    $("#hover6").show();
+});
+$("#hover7").click(function() {
+    $("#show7").show();
+    $("#hover7").hide();
+});
+$("#show7").click(function() {
+    $("#show7").hide();
+    $("#hover7").show();
+});
+// here
 var slideIndex = 0;
 showSlides();
 
