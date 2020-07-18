0ec0d74402c9e1af540a9c0916ae453e354c2123
skuodi <ochiedu27@gmail.com>
2020-07-18 03:45:52 -1200

    Added scrolling detection to trigger menu transition

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -133,6 +133,21 @@ $("#show13").click(function() {
 // Carousel hover popup
 
 // end of carousel hover popup
+//hide menu on scroll********
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
+//*****************
 var slideIndex = 0;
 showSlides();
 
